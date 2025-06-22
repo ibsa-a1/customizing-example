8eb59d29459192ee6c3997e61ab7ea2c2172c58c
ibsa-a1 <ibsaabera712@gmail.com>
2025-06-23 02:48:04 +0300

    First commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
