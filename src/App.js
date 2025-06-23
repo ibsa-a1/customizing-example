1174b83cfc3d8a0c2785e22b8f36f8a831983f46
ibsa-a1 <ibsaabera712@gmail.com>
2025-06-23 03:38:06 +0300

    Footer Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Nav';
+import Promo from './Components/Promo';
+import Intro1 from './Components/Intro1';
+import Intro2 from './Components/Intro2';
+import Intro3 from './Components/Intro3';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Nav />
+    <div className="App">
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+    </div>
   );
 }
 
